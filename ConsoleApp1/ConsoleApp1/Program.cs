afb2e838e9da6200762f20dbf56280a369fee891
Nima <110777961+Nima-sltn@users.noreply.github.com>
2023-12-28 13:03:12 +0330

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number");
+            int x = int.Parse(Console.ReadLine());
+            int y = fibonacci(x - 1);
+
+            if (y < 0)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+                Console.WriteLine(y);
+
+            Console.ReadKey();
+        }
+        static int fibonacci(int x)
+        {
+            if (x <= 1)
+                return x;
+            else if (x > 1)
+                return fibonacci(x - 1) + fibonacci(x - 2);
+            else
+                return -1;
+        }
+    }
+}
